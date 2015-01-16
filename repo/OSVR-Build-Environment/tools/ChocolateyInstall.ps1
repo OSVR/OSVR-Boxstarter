ec861ef057dd529e3b40f881a3e7b3cc90daaf91
Ryan Pavlik <ryan@sensics.com>
2015-01-16 17:08:36 -0600

    Include installing boxstarter itself in the base package.

--- repo/OSVR-Build-Environment/tools/ChocolateyInstall.ps1
+++ repo/OSVR-Build-Environment/tools/ChocolateyInstall.ps1
@@ -7,6 +7,7 @@ try {
     cinst python2
     cinst boost-x64-msvc2013 -source "https://www.myget.org/F/boost/"
     cinst nuget.commandline
+    cinst boxstarter
     Write-ChocolateySuccess 'OSVR-Build-Environment'
 } catch {
   Write-ChocolateyFailure 'OSVR-Build-Environment' $($_.Exception.Message)
